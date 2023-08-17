0867fb1213d0b6a81f25ee0434b7ea0894b98c8a
HieuNF <minhhieudn98@gmail.com>
2023-08-17 15:01:24 +0700

    init source code

---
+++ api/Controllers/CloudAccessController.cs
@@ -0,0 +1,28 @@
+﻿using core.Business;
+using core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace api.Controllers
+{
+    public class CloudAccessController : ApiController
+    {
+        IFileAccess _fileAccess = new FileAccess();
+
+        [HttpGet]
+        [ActionName("create-container")]
+        public IEnumerable<string> CreateContainer()
+        {
+            _fileAccess.CreateContainer("containerzzcc");
+
+            return new List<string>();
+        }
+
+
+    }
+}
